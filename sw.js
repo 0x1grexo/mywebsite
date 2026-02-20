e980248bc81325ddd87d9b378069117dbe22c796
Gregor Opmann <opmanngregor@gmail.com>
2026-02-20 09:43:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'gregor_opmann-portfolio-v1';
+const CACHE_NAME = 'gregor_opmann-portfolio-v2';
+
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -19,9 +20,9 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Caching files');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
@@ -29,23 +30,31 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Clearing old cache');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    Promise.all([
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== CACHE_NAME) {
+              return caches.delete(cache);
+            }
+          })
+        )
+      ),
+      self.clients.claim(),
+    ])
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
